654621b265700b3986898c51fd5c1c38561ea516
nkrce <nikolina.krce@gmail.com>
2025-06-10 23:25:24 +0200

    dodavanje router i pinia

---
+++ src/views/main.vue
@@ -0,0 +1,23 @@
+<script setup>
+ // JS - logika komponente
+</script>
+
+<template>
+  <div class="main">
+    <h1>This is the main page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+
+
+
